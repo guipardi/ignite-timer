a30b5462950d4f6e7a137b179f3d8ee8416a555c
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-23 08:26:30 -0300

    Exemplo styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import reactLogo from './assets/react.svg'
+import { Button } from './Button/Button'
 import viteLogo from '/vite.svg'
 
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+      <>
+        <Button variant='primary' />
+        <Button variant='secondary' />
+        <Button variant='danger' />
+        <Button variant='success' />
+        <Button />
+      </>
   )
 }
 
