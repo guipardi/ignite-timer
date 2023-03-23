b1b72598d383a7ad577ecdff3cb3e37b26d0af1a
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-23 14:20:24 -0300

    Subindo alterações sobre rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button/Button'
-
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
