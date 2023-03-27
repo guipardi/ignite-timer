5b50e1c63adb0b6918b6bb3c738a6131d417b099
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-27 12:30:56 -0300

    Subindo

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -29,7 +29,7 @@ export function History() {
               <td>20 Minutos</td>
               <td>Há 2 meses</td>
               <td>
-                <Status statusColor="green">Concluído</Status>
+                <Status statusColor="yellow">Em andamento</Status>
               </td>
             </tr>
             <tr>
@@ -53,7 +53,7 @@ export function History() {
               <td>20 Minutos</td>
               <td>Há 2 meses</td>
               <td>
-                <Status statusColor="green">Concluído</Status>
+                <Status statusColor="red">Interrompido</Status>
               </td>
             </tr>
             <tr>
