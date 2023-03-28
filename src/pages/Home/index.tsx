48d31c15346a05f8dee0479490576f51660d3284
Guilherme Borges <guilhermepardi@gmail.com>
2023-03-28 09:48:38 -0300

    Utilizando a biblioteca zod
    Não seria necessário.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import {
   CountdownContainer,
   FormContainer,
@@ -12,8 +14,15 @@ import {
 
 // controlled / uncontrelled
 
+const newCycleValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
