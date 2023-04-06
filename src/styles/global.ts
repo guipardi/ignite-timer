d369c2c44d9ec99e83be1b526dee17e3e9528613
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-06 08:57:48 -0300

    Atualização final

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,8 +10,6 @@ export const GlobalStyle = createGlobalStyle`
     box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
   body {
-    background: #333;
-    color: #FFF;
     background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
   }
